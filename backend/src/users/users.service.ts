771d6aa80d51a87f5d1c188b5eb51406a30fc285
Neptuno <kontaktalientrix@gmail.com>
2024-10-10 10:24:47 +0200

    fixed TypeORM module

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {Injectable} from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto';
+import {UpdateUserDto} from './dto/update-user.dto';
+import {Repository} from "typeorm";
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    create(createUserDto: CreateUserDto) {
+        return 'This action adds a new user';
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    findAll() : Promise<User[]> {
+        return this.usersRepository.find();
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} user`;
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
 }
