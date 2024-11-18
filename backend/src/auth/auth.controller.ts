cfb8ad4d8dc3705f4f162796bfeb1a5c5b8a05be
Neptuno <kontaktalientrix@gmail.com>
2024-11-18 13:42:26 +0100

    added public decorator

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards} f
 import {AuthService} from "./auth.service";
 import {AuthDto} from "./dto/authDto";
 import {AuthGuard} from "./auth.guard";
+import {Public} from "../custom-decorators/isPublic";
 
 @Controller('auth')
 export class AuthController {
@@ -10,17 +11,18 @@ export class AuthController {
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
+    @Public()
     signIn(@Body() authDto: AuthDto) {
         return this.authService.signIn(authDto.email, authDto.password);
     }
 
     @HttpCode(HttpStatus.OK)
     @Post('register')
+    @Public()
     signUp(@Body() authDto: AuthDto) {
         return this.authService.signUp(authDto.email, authDto.password);
     }
 
-    @UseGuards(AuthGuard)
     @Get('profile')
     getProfile(@Request() req) {
         return req.user;
