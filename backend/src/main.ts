b7db06786db072027716edff9bf308c154007f31
Neptuno <kontaktalientrix@gmail.com>
2024-12-02 14:21:07 +0100

    added cookies in signIn response

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import {ValidationPipe} from "@nestjs/common";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   });
 
   app.enableCors();
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
