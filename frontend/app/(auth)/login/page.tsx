513debf85248a0188844904420ce3f0ca548265d
neptuno <kontaktalientrix@gmail.com>
2025-01-04 16:02:27 +0100

    added error handle for createProject

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,43 +1,7 @@
-"use client";
-
-import { useState } from "react";
 import Image from "next/image"
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {LoginForm} from "@/app/(auth)/login/form";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState<string>(null);
-  const router = useRouter();
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("http://localhost:3000/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-        credentials: "include",
-      });
-
-      if (response.ok) {
-        router.push("/");
-      } else {
-        const errorData = await response.json();
-        setError(errorData.message || "Login failed");
-      }
-    } catch (error) {
-      setError(error.message || "Login failed");
-      console.log(error)
-    }
-  };
-
   return (
       <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
         <div className="flex items-center justify-center py-12">
