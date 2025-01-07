7267fb06ab1567b1cec97c724085990a2577a2f9
neptuno <kontaktalientrix@gmail.com>
2025-01-07 22:37:07 +0100

    added multer-config-service

--- backend/src/projects/projects.module.ts
+++ backend/src/projects/projects.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
-import { ProjectsController } from './projects.controller';
+import {Module} from '@nestjs/common';
+import {ProjectsService} from './projects.service';
+import {ProjectsController} from './projects.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Project} from "./entities/project.entity";
-import {User} from "../users/entities/user.entity";
 import {UsersModule} from "../users/users.module";
+import {MulterModule} from "@nestjs/platform-express";
+import {MulterConfigService} from '../shared/multer-config-service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]),UsersModule],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
+    imports: [TypeOrmModule.forFeature([Project]), UsersModule,
+        MulterModule.registerAsync({
+            useClass: MulterConfigService
+        })
+    ],
+    controllers: [ProjectsController],
+    providers: [ProjectsService],
 })
-export class ProjectsModule {}
+export class ProjectsModule {
+}
