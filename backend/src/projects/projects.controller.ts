1705a39249e7eff26946a31a9f171852c1c9926a
neptuno <kontaktalientrix@gmail.com>
2025-01-07 21:56:36 +0100

    added FileUploadDto

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,14 +1,35 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, Request} from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    Request,
+    UseInterceptors,
+    UploadedFile
+} from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import {FileInterceptor} from "@nestjs/platform-express";
+import {ApiBody, ApiConsumes} from "@nestjs/swagger";
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto,@Request() req) {
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({type: FileUploadDto})
+  create(
+      @UploadedFile() file: Express.Multer.File,
+      @Body() createProjectDto: CreateProjectDto,
+      @Request() req
+  ) {
+      console.log(file)
     return this.projectsService.create(createProjectDto, req.user.sub);
   }
 
