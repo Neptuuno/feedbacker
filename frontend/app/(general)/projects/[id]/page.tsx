6f7d4b7c4a99655f7e7f93f2ec50f79a03f4bc2f
neptuno <kontaktalientrix@gmail.com>
2025-01-10 19:53:46 +0100

    fixed tabs in project detail

--- frontend/app/(general)/projects/[id]/page.tsx
+++ frontend/app/(general)/projects/[id]/page.tsx
@@ -2,21 +2,7 @@ import {Project} from "@/lib/Entities/Project";
 import {fetchWrapper} from "@/lib/fetchwrapper";
 import Image from "next/image";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {
-    Menubar,
-    MenubarCheckboxItem,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarRadioGroup,
-    MenubarRadioItem,
-    MenubarSeparator,
-    MenubarShortcut,
-    MenubarSub,
-    MenubarSubContent,
-    MenubarSubTrigger,
-    MenubarTrigger,
-} from "@/components/ui/menubar"
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
 import {Separator} from "@/components/ui/separator";
 
 
@@ -34,7 +20,7 @@ export default async function ProjectDetail(
     const project: Project = await getData(params);
 
     return (
-        <div>
+        <Tabs defaultValue="overview" className="">
             <div className="flex justify-between">
                 <div>
                     <Card className="w-64">
@@ -43,95 +29,14 @@ export default async function ProjectDetail(
                             <CardDescription className="text-xl">{project.description}</CardDescription>
                         </CardHeader>
                     </Card>
-                    <Menubar className="my-4">
-                        <MenubarMenu>
-                            <MenubarTrigger>File</MenubarTrigger>
-                            <MenubarContent>
-                                <MenubarItem>
-                                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarItem>
-                                    New Window <MenubarShortcut>⌘N</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarItem disabled>New Incognito Window</MenubarItem>
-                                <MenubarSeparator/>
-                                <MenubarSub>
-                                    <MenubarSubTrigger>Share</MenubarSubTrigger>
-                                    <MenubarSubContent>
-                                        <MenubarItem>Email link</MenubarItem>
-                                        <MenubarItem>Messages</MenubarItem>
-                                        <MenubarItem>Notes</MenubarItem>
-                                    </MenubarSubContent>
-                                </MenubarSub>
-                                <MenubarSeparator/>
-                                <MenubarItem>
-                                    Print... <MenubarShortcut>⌘P</MenubarShortcut>
-                                </MenubarItem>
-                            </MenubarContent>
-                        </MenubarMenu>
-                        <MenubarMenu>
-                            <MenubarTrigger>Edit</MenubarTrigger>
-                            <MenubarContent>
-                                <MenubarItem>
-                                    Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarItem>
-                                    Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarSeparator/>
-                                <MenubarSub>
-                                    <MenubarSubTrigger>Find</MenubarSubTrigger>
-                                    <MenubarSubContent>
-                                        <MenubarItem>Search the web</MenubarItem>
-                                        <MenubarSeparator/>
-                                        <MenubarItem>Find...</MenubarItem>
-                                        <MenubarItem>Find Next</MenubarItem>
-                                        <MenubarItem>Find Previous</MenubarItem>
-                                    </MenubarSubContent>
-                                </MenubarSub>
-                                <MenubarSeparator/>
-                                <MenubarItem>Cut</MenubarItem>
-                                <MenubarItem>Copy</MenubarItem>
-                                <MenubarItem>Paste</MenubarItem>
-                            </MenubarContent>
-                        </MenubarMenu>
-                        <MenubarMenu>
-                            <MenubarTrigger>View</MenubarTrigger>
-                            <MenubarContent>
-                                <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-                                <MenubarCheckboxItem checked>
-                                    Always Show Full URLs
-                                </MenubarCheckboxItem>
-                                <MenubarSeparator/>
-                                <MenubarItem inset>
-                                    Reload <MenubarShortcut>⌘R</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarItem disabled inset>
-                                    Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarSeparator/>
-                                <MenubarItem inset>Toggle Fullscreen</MenubarItem>
-                                <MenubarSeparator/>
-                                <MenubarItem inset>Hide Sidebar</MenubarItem>
-                            </MenubarContent>
-                        </MenubarMenu>
-                        <MenubarMenu>
-                            <MenubarTrigger>Profiles</MenubarTrigger>
-                            <MenubarContent>
-                                <MenubarRadioGroup value="benoit">
-                                    <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
-                                    <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
-                                    <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
-                                </MenubarRadioGroup>
-                                <MenubarSeparator/>
-                                <MenubarItem inset>Edit...</MenubarItem>
-                                <MenubarSeparator/>
-                                <MenubarItem inset>Add Profile...</MenubarItem>
-                            </MenubarContent>
-                        </MenubarMenu>
-                    </Menubar>
+                    <TabsList className="my-4">
+                        <TabsTrigger value="overview">Overview</TabsTrigger>
+                        <TabsTrigger value="forms">Forms</TabsTrigger>
+                        <TabsTrigger value="feedback">Feedback</TabsTrigger>
+                    </TabsList>
                 </div>
-                {project.imagePath &&
+                {
+                    project.imagePath &&
                     <Card className="w-64">
                         <CardContent className="p-3">
                             <Image className="rounded-xl"
@@ -139,9 +44,13 @@ export default async function ProjectDetail(
                                    alt="project image"
                                    width={500} height={500}/>
                         </CardContent>
-                    </Card>}
+                    </Card>
+                }
             </div>
             <Separator className="my-8"/>
-        </div>
+            <TabsContent value="overview">Make changes to your account here.</TabsContent>
+            <TabsContent value="forms">Change your password here.</TabsContent>
+            <TabsContent value="feedback">feedback.</TabsContent>
+        </Tabs>
     )
 }
