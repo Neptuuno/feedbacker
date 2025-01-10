293b885c80fe8b34ea348b690a0df710941838ce
neptuno <kontaktalientrix@gmail.com>
2025-01-10 15:41:56 +0100

    added images in projects grid

--- frontend/app/(general)/projects/page.tsx
+++ frontend/app/(general)/projects/page.tsx
@@ -10,6 +10,7 @@ import {
 import {Project} from "@/lib/Entities/Project";
 import Link from "next/link";
 import {fetchWrapper} from "@/lib/fetchwrapper";
+import Image from "next/image";
 
 async function getProjectData(): Promise<Project[]> {
     const url = `${process.env.API_URL}/projects`;
@@ -20,24 +21,25 @@ export default async function Projects() {
     const projects = await getProjectData()
     return (
         <div className="grid grid-cols-4 gap-4">
-        {
-            projects.map((project: Project) => (
-                <Card key={project.id} className="w-[350px]">
-                    <CardHeader>
-                        <CardTitle>{project.name}</CardTitle>
-                        <CardDescription>{project.description}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
-                    </CardContent>
-                    <CardFooter className="flex justify-between">
-                        <Button variant="outline">Edit</Button>
-                        <Link href={"/projects/" + project.id }><Button>View</Button></Link>
-                    </CardFooter>
-                </Card>
+            {
+                projects.map((project: Project) => (
+                    <Card key={project.id} className="w-[350px]">
+                        <CardHeader>
+                            <CardTitle>{project.name}</CardTitle>
+                            <CardDescription>{project.description}</CardDescription>
+                        </CardHeader>
+                        {project?.imagePath && <CardContent>
+                            <Image className="rounded-xl" src={`${process.env.API_URL}/${project.imagePath}`}
+                                   alt="project image" width={500} height={300}/>
+                        </CardContent>}
+                        <CardFooter className="flex justify-between">
+                            <Button variant="outline">Edit</Button>
+                            <Link href={"/projects/" + project.id}><Button>View</Button></Link>
+                        </CardFooter>
+                    </Card>
 
-            ))
-        }
+                ))
+            }
         </div>
     )
 }
