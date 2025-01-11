5d9df34a9d9f9f6f82b7a26cba7112ea8de2ae2e
neptuno <kontaktalientrix@gmail.com>
2025-01-11 14:37:30 +0100

    added table view

--- frontend/app/(general)/projects/page.tsx
+++ frontend/app/(general)/projects/page.tsx
@@ -7,6 +7,25 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 import {Project} from "@/lib/Entities/Project";
 import Link from "next/link";
 import {fetchWrapper} from "@/lib/fetchwrapper";
@@ -20,26 +39,68 @@ async function getProjectData(): Promise<Project[]> {
 export default async function Projects() {
     const projects = await getProjectData()
     return (
-        <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-            {
-                projects.map((project: Project) => (
-                    <Card key={project.id} className="w-[350px]">
-                        <CardHeader>
-                            <CardTitle>{project.name}</CardTitle>
-                            <CardDescription>{project.description}</CardDescription>
-                        </CardHeader>
-                        {project?.imagePath && <CardContent>
-                            <Image className="rounded-xl" src={`${process.env.API_URL}/${project.imagePath}`}
-                                   alt="project image" width={500} height={300}/>
-                        </CardContent>}
-                        <CardFooter className="flex justify-between">
-                            <Button variant="outline">Edit</Button>
-                            <Link href={"/projects/" + project.id}><Button>View</Button></Link>
-                        </CardFooter>
-                    </Card>
+        <div>
+            <div className="mb-4 flex gap-2">
+                <Link href="/projects/create"><Button>Add new project</Button></Link>
+                <Select defaultValue="grid">
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select view"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Select view</SelectLabel>
+                            <SelectItem value="grid">Grid</SelectItem>
+                            <SelectItem value="table">Table</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+                {
+                    projects.map((project: Project) => (
+                        <Card key={project.id} className="w-[350px]">
+                            <CardHeader>
+                                <CardTitle>{project.name}</CardTitle>
+                                <CardDescription>{project.description}</CardDescription>
+                            </CardHeader>
+                            {project?.imagePath && <CardContent>
+                                <Image className="rounded-xl" src={`${process.env.API_URL}/${project.imagePath}`}
+                                       alt="project image" width={500} height={300}/>
+                            </CardContent>}
+                            <CardFooter className="flex justify-between">
+                                <Button variant="outline">Edit</Button>
+                                <Link href={"/projects/" + project.id}><Button>View</Button></Link>
+                            </CardFooter>
+                        </Card>
 
-                ))
-            }
+                    ))
+                }
+            </div>
+            <Table>
+                <TableCaption>A list of all your projects.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Name</TableHead>
+                        <TableHead>Description</TableHead>
+                        <TableHead>Image</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        projects.map((project: Project) => (
+                            <TableRow key={project.id}>
+                                <TableCell className="font-medium">{project.name}</TableCell>
+                                <TableCell>{project.description}</TableCell>
+                                {project?.imagePath && <TableCell>
+                                    <Image className="rounded-full" src={`${process.env.API_URL}/${project.imagePath}`}
+                                           alt="project image" width={32} height={32}/>
+                                </TableCell>}
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+
         </div>
     )
 }
