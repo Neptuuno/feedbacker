15ffdd9f2ce25d44b66d7305f99897532f753da4
neptuno <kontaktalientrix@gmail.com>
2025-01-12 18:06:12 +0100

    updated createProject dto

--- backend/src/forms/dto/create-form.dto.ts
+++ backend/src/forms/dto/create-form.dto.ts
@@ -1,6 +1,12 @@
+import {Matches, MinLength} from "class-validator";
+
 export class CreateFormDto {
+    @MinLength(2)
     name: string;
+    @MinLength(2)
     title: string;
+    @MinLength(10)
     description: string;
+    @Matches(/^#([0-9A-F]{3}|[0-9A-F]{6})$/i)
     color: string;
 }
