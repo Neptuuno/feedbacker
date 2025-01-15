dd8d2a89fbe7bafbf70e06e22056e65d95c71d65
neptuno <kontaktalientrix@gmail.com>
2025-01-15 22:58:35 +0100

    fixed projectId assosiation on backend

--- backend/src/forms/forms.module.ts
+++ backend/src/forms/forms.module.ts
@@ -3,9 +3,10 @@ import { FormsService } from './forms.service';
 import { FormsController } from './forms.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Form} from "./entities/form.entity";
+import {ProjectsModule} from "../projects/projects.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Form])],
+  imports: [TypeOrmModule.forFeature([Form]),ProjectsModule],
   controllers: [FormsController],
   providers: [FormsService],
 })
--- backend/src/forms/forms.service.ts
+++ backend/src/forms/forms.service.ts
@@ -1,20 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { CreateFormDto } from './dto/create-form.dto';
 import { UpdateFormDto } from './dto/update-form.dto';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {Form} from "./entities/form.entity";
+import {ProjectsService} from "../projects/projects.service";
 
 @Injectable()
 export class FormsService {
   constructor(
       @InjectRepository(Form)
       private formsRepository: Repository<Form>,
+      private projectsService: ProjectsService
   ) {
   }
 
-  create(createFormDto: CreateFormDto) {
-    const form = this.formsRepository.create(createFormDto);
+  async create(createFormDto: CreateFormDto) {
+    const project = await this.projectsService.findOne(createFormDto.projectId);
+    if (!project){
+      throw new NotFoundException(`Project with ID ${createFormDto.projectId} not found`);
+    }
+
+    const form = this.formsRepository.create({...createFormDto, project: project});
     return this.formsRepository.save(form);
   }
 
