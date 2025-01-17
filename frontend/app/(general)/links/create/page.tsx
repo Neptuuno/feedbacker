d00de4aff1a921568d5599e7e21b3e13864c2385
neptuno <kontaktalientrix@gmail.com>
2025-01-17 23:23:44 +0100

    update createLinkForm

--- frontend/app/(general)/links/create/page.tsx
+++ frontend/app/(general)/links/create/page.tsx
@@ -2,6 +2,6 @@ import { CreateLinkForm } from "./form";
 export default function CreateLink() {
     return <div className="lg:max-w-[50%]">
         <h1>Create link</h1>
-        <CreateLinkForm/>
+        <CreateLinkForm searchParams={undefined}/>
     </div>
 }
