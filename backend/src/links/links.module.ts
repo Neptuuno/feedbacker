f0cc361fe0107bb19a15bae216225b63e3e358c0
neptuno <kontaktalientrix@gmail.com>
2025-01-18 00:55:01 +0100

    add form relation when creating

--- backend/src/links/links.module.ts
+++ backend/src/links/links.module.ts
@@ -3,9 +3,10 @@ import { LinksService } from './links.service';
 import { LinksController } from './links.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Link} from "./entities/link.entity";
+import {FormsModule} from "../forms/forms.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Link])],
+  imports: [TypeOrmModule.forFeature([Link]),FormsModule],
   controllers: [LinksController],
   providers: [LinksService],
 })
