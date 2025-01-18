36541493d4352e6a3a21c4861fd16f25c79221cf
neptuno <kontaktalientrix@gmail.com>
2025-01-18 17:12:39 +0100

    add render form by slug page

--- frontend/app/(general)/forms/render/[slug]/page.tsx
+++ frontend/app/(general)/forms/render/[slug]/page.tsx
@@ -2,18 +2,14 @@ import {Form} from "@/lib/Entities/Form";
 import {fetchWrapper} from "@/lib/fetchwrapper";
 
 
-async function getData(params: { slug: string }): Promise<Form> {
-    const url = `${process.env.API_URL}/forms/${(params).slug}`;
+async function getData(slug: string): Promise<Form> {
+    const url = `${process.env.API_URL}/links?slug=${slug}`;
     return await fetchWrapper(url);
 }
 
-export default async function FormRender(
-    props: {
-        params: Promise<{ slug: string }>
-    }
-) {
-    const params = await props.params;
-    const form: Form = await getData(params);
+export default async function FormRender({params}: { params: Promise<{ slug: string }> }) {
+    const slug = (await params).slug;
+    const form: Form = await getData(slug);
 
     return (
         <h1>Form render: {form.name}</h1>
