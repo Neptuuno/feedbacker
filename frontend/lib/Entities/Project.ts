e72231c6127e8a2e25afa2eb76b658b38db8ba49
Adam Peer <kontaktalientrix@gmail.com>
2025-03-13 14:19:25 +0100

    add forms view to project detail

--- frontend/lib/Entities/Project.ts
+++ frontend/lib/Entities/Project.ts
@@ -1,9 +1,11 @@
 import {User} from "@/lib/Entities/User";
+import { Form } from "./Form";
 
 export interface Project {
     id: number
     name: string
     description: string
     imagePath: string,
-    user: User
+    user: User,
+    forms: Form[]
 }
