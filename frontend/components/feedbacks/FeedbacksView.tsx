5765420544dda6ffabd88382473f9a00bf75cf63
Adam Peer <kontaktalientrix@gmail.com>
2025-03-19 09:40:09 +0100

    add grid view

---
+++ frontend/components/feedbacks/FeedbacksView.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import {useState} from "react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import FeedbacksTable from "@/components/feedbacks/FeedbacksTable";
+import { Project } from "@/lib/Entities/Project";
+
+interface FeedbacksViewProps {
+    project: Project;
+}
+
+export default function FormsView({project}: FeedbacksViewProps) {
+    const [view, setView] = useState<"grid" | "table">("grid");
+
+    if (project.forms.length === 0) {
+        return (
+            <div className="flex flex-col gap-4 items-center justify-center h-full">
+                <h3 className="text-2xl font-bold tracking-tight">
+                    You have no forms yet
+                </h3>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            {/* Action Header */}
+            <div className="mb-4 flex gap-2">
+                <Select
+                    defaultValue="grid"
+                    onValueChange={(value) => setView(value as "grid" | "table")}
+                >
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select view"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Select view</SelectLabel>
+                            <SelectItem value="grid">Grid</SelectItem>
+                            <SelectItem value="table">Table</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+
+            {/* Grid View */}
+            {view === "grid" && (
+                <h3>Grid feedback view</h3>
+            )}
+
+            {/* Table View */}
+            {view === "table" && (
+                project.forms.map((form) => (
+                    <FeedbacksTable key={form.id} feedbacks={form.feedbacks} form={form}/>
+                ))
+            )}
+        </div>
+    );
+}
