8a58f8d3c5715530fca790ecaf9855270d089153
Adam Peer <kontaktalientrix@gmail.com>
2025-03-19 14:24:20 +0100

    add digitWithTextCard

---
+++ frontend/components/feedbacks/cards/DigitWithTextCard.tsx
@@ -0,0 +1,16 @@
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import * as React from "react";
+
+export default function DigitWithTextCard({digit, text}: {digit: number, text: string}) {
+    return (
+        <Card className="flex flex-col">
+            <CardHeader className="items-center pb-0">
+                <CardTitle>{text}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col items-center justify-center h-full">
+                <div className="text-7xl font-bold">{digit}</div>
+            </CardContent>
+        </Card>
+    )
+
+}
