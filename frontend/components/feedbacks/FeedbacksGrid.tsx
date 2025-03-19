8f9a10f51bd70dbcabe0d0e90da040eeb774e505
Adam Peer <kontaktalientrix@gmail.com>
2025-03-19 14:24:20 +0100

    add digitWithTextCard

--- frontend/components/feedbacks/FeedbacksGrid.tsx
+++ frontend/components/feedbacks/FeedbacksGrid.tsx
@@ -1,5 +1,6 @@
 import {Project} from "@/lib/Entities/Project";
 import { PieChartComponent } from "@/components/charts/PieChart";
+import DigitWithTextCard from "@/components/feedbacks/cards/DigitWithTextCard";
 
 interface FeedbacksGridProps {
     project: Project;
@@ -10,16 +11,7 @@ export default function FeedbacksGrid({project}: FeedbacksGridProps) {
 
     return (
         <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
-            <PieChartComponent/>
+            {project.chartsData?.totalFeedbacks && <DigitWithTextCard digit={project.chartsData.totalFeedbacks} text={'Total feedbacks'}/>}
             <PieChartComponent/>
         </div>
     )
