1b5b10eac3f61f28bfca737406c32be0ab1afbcc
Adam Peer <kontaktalientrix@gmail.com>
2025-04-10 12:50:13 +0200

    remove search bar

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,9 +1,8 @@
 
 import Link from "next/link";
-import {Home, Menu, MessageSquareDiff, NotepadText, PanelsTopLeft, Search} from "lucide-react";
+import {Menu, MessageSquareDiff, NotepadText, PanelsTopLeft} from "lucide-react";
 import {Button} from "@/components/ui/button";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
-import {Input} from "@/components/ui/input";
 import AccountMenu from "@/components/AccountMenu";
 import {NavLink} from "@/components/navigation/NavLink";
 
@@ -46,16 +45,6 @@ export default function Header() {
                 </SheetContent>
             </Sheet>
             <div className="w-full flex-1">
-                <form>
-                    <div className="relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"/>
-                        <Input
-                            type="search"
-                            placeholder="Search..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                        />
-                    </div>
-                </form>
             </div>
         <AccountMenu/>
         </header>
