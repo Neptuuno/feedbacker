ced7beea516a21f026a99ed4759ff80645717eb8
Adam Peer <kontaktalientrix@gmail.com>
2025-04-17 14:08:58 +0200

    add username to form render

--- frontend/app/(forms-render)/forms/render/[slug]/page.tsx
+++ frontend/app/(forms-render)/forms/render/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import {Form} from "@/lib/Entities/Form";
 import {fetchWrapper} from "@/lib/fetchwrapper";
-import {FileCheck, GalleryVerticalEnd, Link2Off} from "lucide-react";
+import {FileCheck, GalleryVerticalEnd, Link2Off, User} from "lucide-react";
 import {
     Card,
     CardContent,
@@ -32,13 +32,13 @@ export default async function FormRender({params}: { params: Promise<{ slug: str
     return (
         <div style={{ backgroundColor: form.color }} className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 grayscale-[0.5]">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                <a href="#" className="flex items-center gap-2 self-center font-medium">
+                <div className="flex items-center gap-2 self-center font-medium">
                     <div
                         className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                        <GalleryVerticalEnd className="size-4"/>
+                        <User className="size-4"/>
                     </div>
-                    Acme Inc.
-                </a>
+                    { form.user.username }
+                </div>
                 <div>
                     <div className="flex flex-col gap-6">
                         <Card>
